aa45e775a7b1de2eae8afa87a29f9f0805387bdc
Slapicola <lapicolasophia05@gmail.com>
2024-11-15 14:26:05 -0500

    Wrote Temple Photo Album Code

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+const currentyear = document.querySelector("#currentyear");
+
+const today = new Date();
+currentyear.innerHTML = today.getFullYear();
+lastModified.innerHTML = `Last Modified: ${document.lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
